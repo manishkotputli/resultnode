2714f247dc894bb9bc1fe3c25fd0b00bb29bff02
Manish Prajapati <118051360+manishkotputli@users.noreply.github.com>
2026-08-07 01:17:31 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ const appEnv = (process.env.ENV || 'local').trim().toLowerCase();
 const sequelizeEnv =
   appEnv === 'production'
     ? 'production'
-    : 'development';
+    : 'production';
 
 const config = require(path.join(__dirname, '..', 'config', 'config.js'))[sequelizeEnv];
 
