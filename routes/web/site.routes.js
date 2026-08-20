d65f1f0970b8d28bdff88ea89b79810e32cf348c
Manish Prajapati <118051360+manishkotputli@users.noreply.github.com>
2026-08-20 20:24:40 +0530

    update sitemap

--- routes/web/site.routes.js
+++ routes/web/site.routes.js
@@ -2,10 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const siteController = require('../../controllers/web/site.controller');
+const sitemapController = require('../../controllers/web/sitemap.controller');
 
 router.get('/', siteController.home);
 router.get('/category/:slug', siteController.categoryPage);
 router.get('/post/:slug', siteController.postDetail);
 router.get('/go/:linkId', siteController.trackLinkClick);
+
+
+router.get('/sitemap.xml', sitemapController.index);
 
 module.exports = router;
